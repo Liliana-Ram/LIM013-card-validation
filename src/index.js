96e0352aa62a7e3c3936aebfb39569264906f60b
Liliana-Ram <lilirame@gmail.com>
2020-07-16 12:15:23 -0500

    funciones de validar tarjeta js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import validator from './validator.js';
 
+function validarT() {
+
+    let tarjeta= document.getElementById("tarjeta").value;
+
+    let result= isValid("tarjeta");
+    console.log(result);
+
+    let cover=maskify("tarjeta");
+    console.log(cover);
+
+    let message= document.getElementById("mensaje");
+    message.innerHTML+="<p>"+cover+"</p>";
+
+      if(tarjeta==true){
+        message.innerHTML+="<p>Tarjeta válida</p>";
+      }
+
+      else{
+        message.innerHTML+="<p>Tarjeta invalida</p>";
+      }
+
+
 console.log(validator);
